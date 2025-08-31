459c309e2369dd9adc33aa81fd010f5eaae82cdf
senthil <datalensai@gmail.com>
2025-08-31 23:08:21 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/react-aidash",
+    build: {
+    outDir: 'dist', // This is the default, but you can explicitly set it
+  },
 })
